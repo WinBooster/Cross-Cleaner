f464a846ac4a13d822a232a61840273cc23d46c1
Roman Danilov <neci.03@mail.ru>
2024-11-28 15:31:50 +0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -38,7 +38,16 @@ struct CleanerData {
     pub remove_directories: bool,
     pub remove_files: bool
 }
+fn get_steam_directory() {
 
+}
+
+fn get_file_lenght(path: String) {
+    let mut vec = vec![];
+
+    vec.try_reserve_exact(n)?;
+    file.by_ref().take(n).read_to_end(&mut vec)?;
+}
 
 fn main() {
     execute!(
@@ -470,6 +479,344 @@ fn main() {
     };
     database.push(c_users_appdata_local_postman_agent);
     //</editor-fold>
+    //<editor-fold desc="IDA Pro">
+    let c_users_appdata_roaming_hex_rays_ida_pro = CleanerData {
+        path: "C:\\Users\\".to_owned() + username + "\\AppData\\Roaming\\Hex-Rays\\IDA Pro\\*.lst",
+        program: "IDA Pro".parse().unwrap(),
+        files_to_remove: vec![],
+        category: "Cache".parse().unwrap(),
+        remove_directories: false,
+        remove_files: true,
+        directories_to_remove: vec![],
+        remove_all_in_dir: false,
+        remove_directory_after_clean: false,
+        folders_to_remove: vec![],
+    };
+    database.push(c_users_appdata_roaming_hex_rays_ida_pro);
+
+    //</editor-fold>
+    //<editor-fold desc="Xamarin"">
+    let c_users_appdata_local_xamarin_logs = CleanerData {
+        path: "C:\\Users\\".to_owned() + username + "\\AppData\\Local\\Xamarin\\Logs\\**\\*.log",
+        program: "Xamarin".parse().unwrap(),
+        files_to_remove: vec![],
+        category: "Logs".parse().unwrap(),
+        remove_directories: false,
+        remove_files: true,
+        directories_to_remove: vec![],
+        remove_all_in_dir: false,
+        remove_directory_after_clean: false,
+        folders_to_remove: vec![],
+    };
+    database.push(c_users_appdata_local_xamarin_logs);
+    //</editor-fold>
+    //<editor-fold desc="Windscribe"">
+    let c_users_appdata_local_windscribe = CleanerData {
+        path: "C:\\Users\\".to_owned() + username + "\\AppData\\Local\\Windscribe\\Windscribe2\\*.txt",
+        program: "Windscribe".parse().unwrap(),
+        files_to_remove: vec![],
+        category: "Logs".parse().unwrap(),
+        remove_directories: false,
+        remove_files: true,
+        directories_to_remove: vec![],
+        remove_all_in_dir: false,
+        remove_directory_after_clean: false,
+        folders_to_remove: vec![],
+    };
+    database.push(c_users_appdata_local_windscribe);
+    //</editor-fold>
+    //<editor-fold desc="GitHub Desktop"">
+    let c_users_appdata_roaming_github_desktop = CleanerData {
+        path: "C:\\Users\\".to_owned() + username + "\\AppData\\Roaming\\GitHub Desktop\\*.log",
+        program: "GitHub Desktop".parse().unwrap(),
+        files_to_remove: vec![],
+        category: "Logs".parse().unwrap(),
+        remove_directories: false,
+        remove_files: true,
+        directories_to_remove: vec![],
+        remove_all_in_dir: false,
+        remove_directory_after_clean: false,
+        folders_to_remove: vec![],
+    };
+    database.push(c_users_appdata_roaming_github_desktop);
+    let c_users_appdata_roaming_github_desktop_logs = CleanerData {
+        path: "C:\\Users\\".to_owned() + username + "\\AppData\\Roaming\\GitHub Desktop\\logs\\*.log",
+        program: "GitHub Desktop".parse().unwrap(),
+        files_to_remove: vec![],
+        category: "Logs".parse().unwrap(),
+        remove_directories: false,
+        remove_files: true,
+        directories_to_remove: vec![],
+        remove_all_in_dir: false,
+        remove_directory_after_clean: false,
+        folders_to_remove: vec![],
+    };
+    database.push(c_users_appdata_roaming_github_desktop_logs);
+    //</editor-fold>
+    //<editor-fold desc="Panda Security"">
+    let c_programdata_panda_security_pslogs = CleanerData {
+        path: "C:\\ProgramData\\Panda Security\\PSLogs\\*.log".parse().unwrap(),
+        program: "Panda Security".parse().unwrap(),
+        files_to_remove: vec![],
+        category: "Logs".parse().unwrap(),
+        remove_directories: false,
+        remove_files: true,
+        directories_to_remove: vec![],
+        remove_all_in_dir: false,
+        remove_directory_after_clean: false,
+        folders_to_remove: vec![],
+    };
+    database.push(c_programdata_panda_security_pslogs);
+    //</editor-fold>
+    //<editor-fold desc="NetLimiter"">
+    let c_programdata_panda_security_pslogs = CleanerData {
+        path: "C:\\ProgramData\\Locktime\\NetLimiter\\**\\logs\\*.log".parse().unwrap(),
+        program: "NetLimiter".parse().unwrap(),
+        files_to_remove: vec![],
+        category: "Logs".parse().unwrap(),
+        remove_directories: false,
+        remove_files: true,
+        directories_to_remove: vec![],
+        remove_all_in_dir: false,
+        remove_directory_after_clean: false,
+        folders_to_remove: vec![],
+    };
+    database.push(c_programdata_panda_security_pslogs);
+    //</editor-fold>
+    //<editor-fold desc="MiniBin"">
+    let c_program_files_x86_minibin = CleanerData {
+        path: "C:\\Program Files (x86)\\MiniBin\\*.txt".parse().unwrap(),
+        program: "MiniBin".parse().unwrap(),
+        files_to_remove: vec![],
+        category: "Logs".parse().unwrap(),
+        remove_directories: false,
+        remove_files: true,
+        directories_to_remove: vec![],
+        remove_all_in_dir: false,
+        remove_directory_after_clean: false,
+        folders_to_remove: vec![],
+    };
+    database.push(c_program_files_x86_minibin);
+    //</editor-fold>
+    //<editor-fold desc="Brave Browser"">
+    let c_program_files_brave_software_brave_browser_application = CleanerData {
+        path: "C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\*.log".parse().unwrap(),
+        program: "Brave Browser".parse().unwrap(),
+        files_to_remove: vec![],
+        category: "Logs".parse().unwrap(),
+        remove_directories: false,
+        remove_files: true,
+        directories_to_remove: vec![],
+        remove_all_in_dir: false,
+        remove_directory_after_clean: false,
+        folders_to_remove: vec![],
+    };
+    database.push(c_program_files_brave_software_brave_browser_application);
+    let c_users_appdata_local_brave_software_brave_browser_user_data_default = CleanerData {
+        path: "C:\\Users\\".to_owned() + username+ "\\AppData\\Local\\BraveSoftware\\Brave-Browser\\User Data\\Default",
+        program: "Brave Browser".parse().unwrap(),
+        files_to_remove: vec![
+            "Favicons".parse().unwrap(),
+            "Favicons-journal".parse().unwrap(),
+            "History".parse().unwrap(),
+            "History-journal".parse().unwrap(),
+            "Visited Links".parse().unwrap()
+        ],
+        category: "Cache".parse().unwrap(),
+        remove_directories: false,
+        remove_files: false,
+        directories_to_remove: vec![],
+        remove_all_in_dir: false,
+        remove_directory_after_clean: false,
+        folders_to_remove: vec![],
+    };
+    database.push(c_users_appdata_local_brave_software_brave_browser_user_data_default);
+    let c_users_appdata_local_brave_software_brave_browser_user_data_default_dawn_cache = CleanerData {
+        path: "C:\\Users\\".to_owned() + username +"\\AppData\\Local\\BraveSoftware\\Brave-Browser\\User Data\\Default\\DawnCache",
+        program: "Brave Browser".parse().unwrap(),
+        files_to_remove: vec![
+            "data_0".parse().unwrap(),
+            "data_1".parse().unwrap(),
+            "data_2".parse().unwrap(),
+            "data_3".parse().unwrap(),
+            "index".parse().unwrap()
+        ],
+        category: "Cache".parse().unwrap(),
+        remove_directories: false,
+        remove_files: false,
+        directories_to_remove: vec![],
+        remove_all_in_dir: false,
+        remove_directory_after_clean: false,
+        folders_to_remove: vec![],
+    };
+    database.push(c_users_appdata_local_brave_software_brave_browser_user_data_default_dawn_cache);
+    let c_users_appdata_local_brave_software_brave_browser_user_data_default_gpu_cache = CleanerData {
+        path: "C:\\Users\\".to_owned() + username +"\\AppData\\Local\\BraveSoftware\\Brave-Browser\\User Data\\Default\\GPUCache",
+        program: "Brave Browser".parse().unwrap(),
+        files_to_remove: vec![
+            "data_0".parse().unwrap(),
+            "data_1".parse().unwrap(),
+            "data_2".parse().unwrap(),
+            "data_3".parse().unwrap(),
+            "index".parse().unwrap()
+        ],
+        category: "Cache".parse().unwrap(),
+        remove_directories: false,
+        remove_files: false,
+        directories_to_remove: vec![],
+        remove_all_in_dir: false,
+        remove_directory_after_clean: false,
+        folders_to_remove: vec![],
+    };
+    database.push(c_users_appdata_local_brave_software_brave_browser_user_data_default_gpu_cache);
+    //</editor-fold>
+    //<editor-fold desc="Mem Reduct"">
+    let c_program_files_brave_software_brave_browser_application = CleanerData {
+        path: "C:\\Program Files\\Mem Reduct".parse().unwrap(),
+        program: "Mem Reduct".parse().unwrap(),
+        files_to_remove: vec![
+            "History.txt".parse().unwrap(),
+            "License.txt".parse().unwrap(),
+            "Readme.txt".parse().unwrap(),
+            "memreduct.exe.sig".parse().unwrap()
+        ],
+        category: "Logs".parse().unwrap(),
+        remove_directories: false,
+        remove_files: false,
+        directories_to_remove: vec![],
+        remove_all_in_dir: false,
+        remove_directory_after_clean: false,
+        folders_to_remove: vec![],
+    };
+    database.push(c_program_files_brave_software_brave_browser_application);
+    //</editor-fold>
+    //<editor-fold desc="qBittorrent"">
+    let c_program_files_qbittorent = CleanerData {
+        path: "C:\\Program Files\\qBittorrent\\*.pdb".parse().unwrap(),
+        program: "qBittorrent".parse().unwrap(),
+        files_to_remove: vec![],
+        category: "Logs".parse().unwrap(),
+        remove_directories: false,
+        remove_files: true,
+        directories_to_remove: vec![],
+        remove_all_in_dir: false,
+        remove_directory_after_clean: false,
+        folders_to_remove: vec![],
+    };
+    database.push(c_program_files_qbittorent);
+    let c_program_files_qbittorent_logs = CleanerData {
+        path: "C:\\Program Files\\qBittorrent\\logs\\*.log".parse().unwrap(),
+        program: "qBittorrent".parse().unwrap(),
+        files_to_remove: vec![],
+        category: "Logs".parse().unwrap(),
+        remove_directories: false,
+        remove_files: true,
+        directories_to_remove: vec![],
+        remove_all_in_dir: false,
+        remove_directory_after_clean: false,
+        folders_to_remove: vec![],
+    };
+    database.push(c_program_files_qbittorent_logs);
+    //</editor-fold>
+    //<editor-fold desc="CCleaner"">
+    let c_program_files_ccleaner_logs = CleanerData {
+        path: "C:\\Program Files\\CCleaner\\LOG\\*".parse().unwrap(),
+        program: "CCleaner".parse().unwrap(),
+        files_to_remove: vec![],
+        category: "Logs".parse().unwrap(),
+        remove_directories: false,
+        remove_files: true,
+        directories_to_remove: vec![],
+        remove_all_in_dir: false,
+        remove_directory_after_clean: false,
+        folders_to_remove: vec![],
+    };
+    database.push(c_program_files_ccleaner_logs);
+    //</editor-fold>
+    //<editor-fold desc="IObit Malware Fighter"">
+    let c_program_files_ccleaner_logs = CleanerData {
+        path: "C:\\ProgramData\\IObit\\IObit Malware Fighter\\*.log".parse().unwrap(),
+        program: "IObit Malware Fighter".parse().unwrap(),
+        files_to_remove: vec![],
+        category: "Logs".parse().unwrap(),
+        remove_directories: false,
+        remove_files: true,
+        directories_to_remove: vec![],
+        remove_all_in_dir: false,
+        remove_directory_after_clean: false,
+        folders_to_remove: vec![],
+    };
+    database.push(c_program_files_ccleaner_logs);
+    let c_program_data_iobit_iobit_malware_finghter_homepage_advisor = CleanerData {
+        path: "C:\\ProgramData\\IObit\\IObit Malware Fighter\\Homepage Advisor\\*.log".parse().unwrap(),
+        program: "IObit Malware Fighter".parse().unwrap(),
+        files_to_remove: vec![],
+        category: "Logs".parse().unwrap(),
+        remove_directories: false,
+        remove_files: true,
+        directories_to_remove: vec![],
+        remove_all_in_dir: false,
+        remove_directory_after_clean: false,
+        folders_to_remove: vec![],
+    };
+    database.push(c_program_data_iobit_iobit_malware_finghter_homepage_advisor);
+    //</editor-fold>
+    //<editor-fold desc="IObit Driver Booster"">
+    let c_users_appdata_roaming_iobit_driver_booster_logs = CleanerData {
+        path: "C:\\Users\\".to_owned() + username + "\\AppData\\Roaming\\IObit\\Driver Booster\\Logs\\*",
+        program: "IObit Driver Booster".parse().unwrap(),
+        files_to_remove: vec![],
+        category: "Logs".parse().unwrap(),
+        remove_directories: true,
+        remove_files: true,
+        directories_to_remove: vec![],
+        remove_all_in_dir: false,
+        remove_directory_after_clean: false,
+        folders_to_remove: vec![],
+    };
+    database.push(c_users_appdata_roaming_iobit_driver_booster_logs);
+    let c_program_files_x86_iobit_driver_booster = CleanerData {
+        path: "C:\\Program Files (x86)\\IObit\\Driver Booster\\*.log".parse().unwrap(),
+        program: "IObit Driver Booster".parse().unwrap(),
+        files_to_remove: vec![],
+        category: "Logs".parse().unwrap(),
+        remove_directories: false,
+        remove_files: true,
+        directories_to_remove: vec![],
+        remove_all_in_dir: false,
+        remove_directory_after_clean: false,
+        folders_to_remove: vec![],
+    };
+    database.push(c_program_files_x86_iobit_driver_booster);
+    let c_program_files_x86_iobit_driver_booster_1 = CleanerData {
+        path: "C:\\Program Files (x86)\\IObit\\Driver Booster\\*.txt".parse().unwrap(),
+        program: "IObit Driver Booster".parse().unwrap(),
+        files_to_remove: vec![],
+        category: "Logs".parse().unwrap(),
+        remove_directories: false,
+        remove_files: true,
+        directories_to_remove: vec![],
+        remove_all_in_dir: false,
+        remove_directory_after_clean: false,
+        folders_to_remove: vec![],
+    };
+    database.push(c_program_files_x86_iobit_driver_booster_1);
+    //</editor-fold>
+    //<editor-fold desc="Process Lasso"">
+    let c_program_data_process_lasso_logs = CleanerData {
+        path: "C:\\ProgramData\\ProcessLasso\\logs\\*".parse().unwrap(),
+        program: "Process Lasso".parse().unwrap(),
+        files_to_remove: vec![],
+        category: "Logs".parse().unwrap(),
+        remove_directories: false,
+        remove_files: true,
+        directories_to_remove: vec![],
+        remove_all_in_dir: false,
+        remove_directory_after_clean: false,
+        folders_to_remove: vec![],
+    };
+    database.push(c_program_data_process_lasso_logs);
+    //</editor-fold>
 
     //<editor-fold desc="Images">
 
@@ -522,6 +869,7 @@ fn main() {
         .with_formatter(formatter)
         .prompt();
 
+    let mut bytes_cleared = 0;
     let mut removed_files = 0;
     let mut removed_directories = 0;
     let mut cleared_programs:Vec<Cleared> = vec![];
@@ -542,6 +890,7 @@ fn main() {
                                         let is_file: bool = result.is_file();
                                         let path: &str = result.as_path().to_str().unwrap();
                                         let name: Option<&str> = result.file_name().unwrap().to_str();
+                                        let lenght = result.metadata().unwrap().len();
                                         //println!("Found: {}", path);
                                         for file in &data.files_to_remove {
                                             let file_path = path.to_owned() + "\\" + &*file;
@@ -549,6 +898,7 @@ fn main() {
                                                 Ok(_) => {
                                                     working = true;
                                                     removed_files += 1;
+                                                    bytes_cleared += lenght;
                                                     //println!("Removed file: {}", name.unwrap());
                                                 }
                                                 Err(_) => {}
@@ -560,6 +910,7 @@ fn main() {
                                                 Ok(_) => {
                                                     working = true;
                                                     removed_directories += 1;
+                                                    bytes_cleared += lenght;
                                                     //println!("Removed file: {}", name.unwrap());
                                                 }
                                                 Err(_) => {}
@@ -571,6 +922,7 @@ fn main() {
                                             match fs::remove_dir(dir_path) {
                                                 Ok(_) => {
                                                     removed_directories += 1;
+                                                    bytes_cleared += lenght;
                                                     working = true;
                                                     //println!("Removed directory: {}", name.unwrap());
                                                 }
@@ -583,6 +935,7 @@ fn main() {
                                             match fs::remove_file(path) {
                                                 Ok(_) => {
                                                     removed_files += 1;
+                                                    bytes_cleared += lenght;
                                                     working = true;
                                                     //println!("Removed file: {}", name.unwrap());
                                                 }
@@ -593,6 +946,7 @@ fn main() {
                                             match fs::remove_dir_all(path) {
                                                 Ok(_) => {
                                                     removed_directories += 1;
+                                                    bytes_cleared += lenght;
                                                     working = true;
                                                     //println!("Removed directory: {}", name.unwrap());
                                                 }
@@ -605,6 +959,7 @@ fn main() {
                                             match fs::remove_dir_all(path) {
                                                 Ok(_) => {
                                                     working = true;
+                                                    bytes_cleared += lenght;
                                                     removed_files += results.unwrap().count();
                                                 }
                                                 Err(_) => {}
