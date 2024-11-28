acacbade1fcb8928c22b69887694f5d27cff4efd
Roman Danilov <neci.03@mail.ru>
2024-11-28 20:33:56 +0500

    Async

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::ffi::OsStr;
+use std::ffi::{CString, OsStr};
 use std::fmt::{format, Debug};
 use std::fs;
 use std::io::stdin;
@@ -38,8 +38,147 @@ struct CleanerData {
     pub remove_directories: bool,
     pub remove_files: bool
 }
+struct CleanerResult {
+    pub files: u64,
+    pub folders: u64,
+    pub bytes: u64,
+    pub working: bool,
+    pub program: String
+}
 fn get_steam_directory() {
 
+}
+
+fn clear_category(data: &CleanerData) -> CleanerResult{
+    let mut cleaner_result: CleanerResult = CleanerResult { files: 0, folders: 0, bytes: 0, working: false, program: "".parse().unwrap() };
+    let results: Result<Paths, PatternError> = glob(&*data.path);
+    cleaner_result.program = (&*data.program).parse().unwrap();
+    match results {
+        Ok(results) => {
+            for result in results {
+                match result {
+                    Ok(result) => {
+                        let is_dir: bool = result.is_dir();
+                        let is_file: bool = result.is_file();
+                        let path: &str = result.as_path().to_str().unwrap();
+                        let name: Option<&str> = result.file_name().unwrap().to_str();
+                        let mut lenght = 0;
+                        match result.metadata() {
+                            Ok(res) => { lenght += res.len(); }
+                            Err(_) => {}
+                        }
+                        //println!("Found: {}", path);
+                        for file in &data.files_to_remove {
+                            let file_path = path.to_owned() + "\\" + &*file;
+                            match fs::remove_file(file_path) {
+                                Ok(_) => {
+                                    cleaner_result.files += 1;
+                                    cleaner_result.bytes += lenght;
+                                    cleaner_result.working = true;
+                                    //println!("Removed file: {}", name.unwrap());
+                                }
+                                Err(_) => {}
+                            }
+                        }
+                        for directory in &data.directories_to_remove {
+                            let file_path = path.to_owned() + "\\" + &*directory;
+                            let metadata = fs::metadata(file_path.clone());
+                            match metadata {
+                                Ok(res) => { lenght += res.len(); }
+                                Err(_) => {}
+                            }
+                            match fs::remove_dir_all(file_path) {
+                                Ok(_) => {
+                                    cleaner_result.folders += 1;
+                                    cleaner_result.working = true;
+                                    //println!("Removed file: {}", name.unwrap());
+                                }
+                                Err(_) => {}
+                            }
+                        }
+
+                        for dir in &data.directories_to_remove {
+                            let dir_path = path.to_owned() + "\\" + &*dir;
+                            let metadata = fs::metadata(dir_path.clone());
+                            match metadata {
+                                Ok(res) => { lenght += res.len(); }
+                                Err(_) => {}
+                            }
+                            match fs::remove_dir(dir_path) {
+                                Ok(_) => {
+                                    cleaner_result.folders += 1;
+                                    cleaner_result.bytes += lenght;
+                                    cleaner_result.working = true;
+                                    //println!("Removed directory: {}", name.unwrap());
+                                }
+                                Err(_) => {}
+                            }
+                        }
+
+                        //println!("Found: {}", path);
+                        if data.remove_files && is_file {
+                            match fs::remove_file(path) {
+                                Ok(_) => {
+                                    cleaner_result.files += 1;
+                                    cleaner_result.bytes += lenght;
+                                    cleaner_result.working = true;
+                                    //println!("Removed file: {}", name.unwrap());
+                                }
+                                Err(_) => {}
+                            }
+                        }
+                        if data.remove_directories && is_dir {
+                            match fs::remove_dir_all(path) {
+                                Ok(_) => {
+                                    cleaner_result.folders += 1;
+                                    cleaner_result.bytes += lenght;
+                                    cleaner_result.working = true;
+                                    //println!("Removed directory: {}", name.unwrap());
+                                }
+                                Err(_) => {}
+                            }
+                        }
+                        if data.remove_all_in_dir {
+                            let results: Result<Paths, PatternError> = glob(&*(path.to_owned() + "\\*"));
+                            let mut files = 0;
+                            let mut dirs = 0;
+                            match results {
+                                Ok(results) => {
+                                    for result in results {
+                                        match result {
+                                            Ok(result) => {
+                                                if result.is_file() {
+                                                    files += 1;
+                                                }
+                                                if result.is_dir() {
+                                                    dirs += 1;
+                                                }
+                                            }
+                                            Err(_) => {}
+                                        }
+                                    }
+                                    match fs::remove_dir_all(path) {
+                                        Ok(_) => {
+                                            cleaner_result.files += files;
+                                            cleaner_result.folders += dirs;
+                                            cleaner_result.bytes += lenght;
+                                            cleaner_result.working = true;
+                                        }
+                                        Err(_) => {}
+                                    }
+                                }
+                                Err(_) => {}
+                            }
+
+                        }
+                    }
+                    Err(_) => {}
+                }
+            }
+        }
+        Err(_) => {}
+    }
+    return cleaner_result;
 }
 
 fn get_file_size_string(size: u64) -> String {
@@ -55,10 +194,11 @@ fn get_file_size_string(size: u64) -> String {
 }
 
 
-fn main() {
+#[tokio::main]
+async fn main() {
     execute!(
         std::io::stdout(),
-        crossterm::terminal::SetTitle("WinBooster CLI v1.0.4")
+        crossterm::terminal::SetTitle("WinBooster CLI v1.0.5")
     );
 
     let username = &*whoami::username();
@@ -2692,144 +2832,29 @@ fn main() {
 
     match ans {
         Ok(ans) => {
+            let mut async_list = vec![];
             for data in database.iter().clone() {
-                let mut working = false;
                 if ans.contains(&&*data.category) {
-                    let results: Result<Paths, PatternError> = glob(&*data.path);
-                    match results {
-                        Ok(results) => {
-                            for result in results {
-                                match result {
-                                    Ok(result) => {
-                                        let is_dir: bool = result.is_dir();
-                                        let is_file: bool = result.is_file();
-                                        let path: &str = result.as_path().to_str().unwrap();
-                                        let name: Option<&str> = result.file_name().unwrap().to_str();
-                                        let mut lenght = 0;
-                                        match result.metadata() {
-                                            Ok(res) => { lenght += res.len(); }
-                                            Err(_) => {}
-                                        }
-                                        //println!("Found: {}", path);
-                                        for file in &data.files_to_remove {
-                                            let file_path = path.to_owned() + "\\" + &*file;
-                                            match fs::remove_file(file_path) {
-                                                Ok(_) => {
-                                                    working = true;
-                                                    removed_files += 1;
-                                                    bytes_cleared += lenght;
-                                                    //println!("Removed file: {}", name.unwrap());
-                                                }
-                                                Err(_) => {}
-                                            }
-                                        }
-                                        for directory in &data.directories_to_remove {
-                                            let file_path = path.to_owned() + "\\" + &*directory;
-                                            let metadata = fs::metadata(file_path.clone());
-                                            match metadata {
-                                                Ok(res) => { lenght += res.len(); }
-                                                Err(_) => {}
-                                            }
-                                            match fs::remove_dir_all(file_path) {
-                                                Ok(_) => {
-                                                    working = true;
-                                                    removed_directories += 1;
-                                                    bytes_cleared += lenght;
-                                                    //println!("Removed file: {}", name.unwrap());
-                                                }
-                                                Err(_) => {}
-                                            }
-                                        }
-
-                                        for dir in &data.directories_to_remove {
-                                            let dir_path = path.to_owned() + "\\" + &*dir;
-                                            let metadata = fs::metadata(dir_path.clone());
-                                            match metadata {
-                                                Ok(res) => { lenght += res.len(); }
-                                                Err(_) => {}
-                                            }
-                                            match fs::remove_dir(dir_path) {
-                                                Ok(_) => {
-                                                    removed_directories += 1;
-                                                    bytes_cleared += lenght;
-                                                    working = true;
-                                                    //println!("Removed directory: {}", name.unwrap());
-                                                }
-                                                Err(_) => {}
-                                            }
-                                        }
-
-                                        //println!("Found: {}", path);
-                                        if data.remove_files && is_file {
-                                            match fs::remove_file(path) {
-                                                Ok(_) => {
-                                                    removed_files += 1;
-                                                    bytes_cleared += lenght;
-                                                    working = true;
-                                                    //println!("Removed file: {}", name.unwrap());
-                                                }
-                                                Err(_) => {}
-                                            }
-                                        }
-                                        if data.remove_directories && is_dir {
-                                            match fs::remove_dir_all(path) {
-                                                Ok(_) => {
-                                                    removed_directories += 1;
-                                                    bytes_cleared += lenght;
-                                                    working = true;
-                                                    //println!("Removed directory: {}", name.unwrap());
-                                                }
-                                                Err(_) => {}
-                                            }
-                                        }
-                                        if data.remove_all_in_dir {
-                                            let results: Result<Paths, PatternError> = glob(&*(path.to_owned() + "\\*"));
-                                            let mut files = 0;
-                                            let mut dirs = 0;
-                                            match results {
-                                                Ok(results) => {
-                                                    for result in results {
-                                                        match result {
-                                                            Ok(result) => {
-                                                                if result.is_file() {
-                                                                    files += 1;
-                                                                }
-                                                                if result.is_dir() {
-                                                                    dirs += 1;
-                                                                }
-                                                            }
-                                                            Err(_) => {}
-                                                        }
-                                                    }
-                                                    match fs::remove_dir_all(path) {
-                                                        Ok(_) => {
-                                                            working = true;
-                                                            bytes_cleared += lenght;
-                                                            removed_files += files;
-                                                            removed_directories += dirs;
-                                                        }
-                                                        Err(_) => {}
-                                                    }
-                                                }
-                                                Err(_) => {}
-                                            }
-
-                                        }
-                                    }
-                                    Err(_) => {}
-                                }
-                            }
-                        }
-                        Err(_) => {}
-                    }
+                    let asyncs = async {
+                        let result = clear_category(data);
+                        return result;
+                    };
+                    async_list.push(asyncs);
                 }
-                let mut program = data.program.clone();
-                if working {
-                    let data = Cleared { Program: program };
+            }
+
+            for asyncscc in async_list {
+                let result = asyncscc.await;
+                removed_files += result.files;
+                removed_directories += result.folders;
+                bytes_cleared += result.bytes;
+                if result.working {
+                    let data = Cleared { Program: result.program };
                     if !cleared_programs.contains(&data) {
                         cleared_programs.push(data);
                     }
                 }
+
             }
         },
         Err(_) => println!("Can't work with these categories"),
