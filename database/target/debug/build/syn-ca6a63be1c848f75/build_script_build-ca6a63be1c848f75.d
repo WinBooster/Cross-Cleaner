d61224e749c2b3deb2d5944e149b014dc9045bd4
roman <neci.03@mail.ru>
2025-03-26 03:28:18 +0500

    Remove not used trash

--- database/target/debug/build/syn-ca6a63be1c848f75/build_script_build-ca6a63be1c848f75.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Administrator\Documents\GitHub\WinBooster-CLI\database\target\debug\build\syn-ca6a63be1c848f75\build_script_build-ca6a63be1c848f75.exe: C:\Users\Administrator\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\syn-1.0.109\build.rs
-
-C:\Users\Administrator\Documents\GitHub\WinBooster-CLI\database\target\debug\build\syn-ca6a63be1c848f75\build_script_build-ca6a63be1c848f75.d: C:\Users\Administrator\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\syn-1.0.109\build.rs
-
-C:\Users\Administrator\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\syn-1.0.109\build.rs:
