855954f8ee45575377aa3070b8e65a31c1bbb5d2
Roman Danilov <neci.03@mail.ru>
2025-06-13 06:46:53 +0500

    Improve?

--- database/src/cleaner_database.rs
+++ database/src/cleaner_database.rs
@@ -45,7 +45,7 @@ pub fn get_default_database() -> &'static Vec<CleanerData> {
             // Replace {username}
             new_entry.path = new_entry.path.replace("{username}", &username);
 
-            // Replace {steam}
+            // Replace {steam} (Windows only)
             new_entry.path = new_entry.path.replace("{steam}", &steam_directory);
 
             // Replace {drive} (Windows only)
@@ -71,5 +71,45 @@ pub fn get_database_from_file(file_path: &str) -> Result<Vec<CleanerData>, Box<d
     // Deserialization JSON to Vec<CleanerData>
     let database: Vec<CleanerData> = serde_json::from_str(&data)?;
 
-    Ok(database)
+	// Get the username
+    let username = whoami::username();
+
+	// Getting a list of disks (Windows only)
+    let drives = if cfg!(windows) {
+        get_letters()
+    } else {
+        vec![] // Linux does not use disks
+    };
+
+    // Get the path to Steam
+    let steam_directory = if cfg!(windows) {
+        get_steam_directory_from_registry()
+    } else {
+        String::new() // Not used on Linux
+    };
+
+	let mut expanded_database = Vec::new();
+
+	for entry in database {
+        let mut new_entry = entry.clone();
+
+        // Replace {username}
+        new_entry.path = new_entry.path.replace("{username}", &username);
+
+        // Replace {steam} (Windows only)
+        new_entry.path = new_entry.path.replace("{steam}", &steam_directory);
+
+        // Replace {drive} (Windows only)
+        if cfg!(windows) && new_entry.path.contains("{drive}") {
+            for drive in &drives {
+                let mut drive_entry = new_entry.clone();
+                drive_entry.path = drive_entry.path.replace("{drive}", drive);
+                expanded_database.push(drive_entry);
+            }
+        } else {
+            expanded_database.push(new_entry);
+        }
+    }
+
+    Ok(expanded_database)
 }
